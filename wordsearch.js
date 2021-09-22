cbc3f8b1d3f6a618c7972ed8c7fdeb7bfad2eda9
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-22 20:55:24 +0000

    love the search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
+const trans = require("/vagrant/w2/d2/ThereIsNoSpoon.js");
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {return true}
     }
-}
+    
+    let newLetters = trans(letters); 
+    // console.log(newLetters);
+    const verticalJoin = newLetters.map(ls => ls.join(''));
+    // console.log(verticalJoin);  // correct line 
+    for (i of verticalJoin) {
+        if (i.includes(word)) 
+            {return true}
+        }
+    return false 
+    }
 
 module.exports = wordSearch
